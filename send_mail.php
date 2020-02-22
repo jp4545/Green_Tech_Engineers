eb07a693c125cb70248b8c8659c9649ed6fe8dfd
jp4545 <prakashj1998@gmail.com>
2020-02-22 13:11:49 +0530

    First commit

--- send_mail.php
+++ send_mail.php
@@ -1,7 +1,15 @@
 <?php
-    $mailto = $_POST['mail_to'];
-    $mailSub = $_POST['mail_sub'];
-    $mailMsg = $_POST['mail_msg'];
+    $f_name = $_POST['f_name'];
+    $l_name = $_POST['l_name'];
+    $mailto = $_POST['u_email'];
+    $c_name = $_POST['c_name'];
+    $f_name = $_POST['f_name'];
+    $u_contact = $_POST['u_contact'];
+    $u_message = $_POST['u_message'];
+    $u_init = "Hii, I am ";
+    $message = "Hi, I am $f_name $l_name from $c_name. My Message : $u_message My Contact : $u_contact";
+    echo "Hi, I am $f_name $l_name from $c_name. My Message : $u_message. My Contact : $u_contact";
+    $mailSub = "Test Mail";
    require 'PHPMailer-master/PHPMailerAutoload.php';
    $mail = new PHPMailer();
    $mail ->IsSmtp();
@@ -11,11 +19,11 @@
    $mail ->Host = "smtp.gmail.com";
    $mail ->Port = 465; // or 587
    $mail ->IsHTML(true);
-   $mail ->Username = "yourmail@gmail.com";
-   $mail ->Password = "yourpassword";
-   $mail ->SetFrom("yourmail@gmail.com");
+   $mail ->Username = "prakashj1998@gmail.com";
+   $mail ->Password = "SweetyMalar.123";
+   $mail ->SetFrom("prakashj1998@gmail.com");
    $mail ->Subject = $mailSub;
-   $mail ->Body = $mailMsg;
+   $mail ->Body = $message;
    $mail ->AddAddress($mailto);
 
    if(!$mail->Send())
@@ -24,8 +32,9 @@
    }
    else
    {
-       echo "Mail Sent";
+       alert "Mail Sent";
    }
+    ?>
 
 
 
